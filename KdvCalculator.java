6f8fb5bf94e34849cc463f66599b35ccf7c808bd
e. berke aydin <71606941+eberkeaydin@users.noreply.github.com>
2021-10-29 22:57:13 +0300

    project committed
    Patika Dev | Java101 | KDV Tutarı Hesaplayan Program

---
+++ KdvCalculator.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+            Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+            (Not : KDV tutarını 18% olarak alın)
+            KDV'siz Fiyat = 10;
+            KDV'li Fiyat = 11.8;
+            KDV tutarı = 1.8;
+            Ödev :
+            Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 , tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.
+         */
+
+        float fee;
+        float KDV = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the fee : ");
+        fee = scan.nextInt();
+
+        if (fee > 0 && fee < 1000){
+            KDV = 0.18F;
+        }
+        else if (fee >= 1000) {
+            KDV = 0.08F;
+        }
+
+        System.out.println("KDV fee is : " + (fee * KDV));
+        }
+
+    }
+
+
+
