ccc68d2b5394c244ba42ee829a72d0fe53a8f82e
e. berke aydin <71606941+eberkeaydin@users.noreply.github.com>
2021-11-17 21:54:14 +0300

    create the project

---
+++ recursivePowerFunc.java
@@ -0,0 +1,27 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int base = 3, powerRaised = 4;
+        int result = power(base, powerRaised);
+
+        System.out.println(base + "^" + powerRaised + "=" + result);
+    }
+
+    public static int power(int base, int powerRaised) {
+        if (powerRaised != 0) {
+
+            return (base * power(base, powerRaised - 1));
+        }
+        else {
+            return 1;
+        }
+    }
+
+
+}
+
+
+
+
+
